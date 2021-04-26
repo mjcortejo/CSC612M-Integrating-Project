99e47bfdf6f1bc2eec6fe3b82cd1704becbb2385
Mark Cortejo <mark_jethro27@yahoo.com>
2021-04-26 04:56:27 -0700

    chore: Add initial forms for GUI design

---
+++ src/csc612m/integrating/project/CSC612MIntegratingProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc612m.integrating.project;
+
+/**
+ *
+ * @author mark
+ */
+public class CSC612MIntegratingProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
