41a07883f0d4221c5b7b3f7d68f282b938880d85
Mark Cortejo <mark_jethro27@yahoo.com>
2021-05-25 04:43:07 -0700

    fix: implement instruction fetch and decode

--- src/csc612m/integrating/project/Pipeline.java
+++ src/csc612m/integrating/project/Pipeline.java
@@ -21,23 +21,64 @@ public class Pipeline {
     
     JTable tableRegister;
     JTable tableProgram;
+    JTable tablePipelineMap;
+    JTable tablePipelineInternalRegister;
+    
     HashMap<String, Integer> register_alias_map;
+    HashMap<String, String> pipeline_map;
+    HashMap<String, Integer> pipeline_internal_register_map;
+    
     DefaultTableModel register_model;
     DefaultTableModel program_model;
+    DefaultTableModel pipeline_map_model;
+    DefaultTableModel pipeline_internal_register_model;
     
-    public Pipeline(JTable jTableRegister, JTable jTableProgram)
+    int cycles = 0;
+    
+    String IF = "";
+    String ID = "";
+    String EX = "";
+    String MEM = "";
+    String WB = "";
+    
+    public Pipeline(JTable jTableRegister, JTable jTableProgram, JTable jTablePipelineMap, JTable jTablePipelineRegister)
     {
         tableRegister = jTableRegister;
         tableProgram = jTableProgram;
+        tablePipelineMap = jTablePipelineMap;
+        tablePipelineInternalRegister = jTablePipelineRegister;
+        
         register_model = (DefaultTableModel)tableRegister.getModel();
         program_model = (DefaultTableModel)tableProgram.getModel();
+        pipeline_map_model = (DefaultTableModel) jTablePipelineMap.getModel();
+        pipeline_internal_register_model = (DefaultTableModel) jTablePipelineRegister.getModel();
         
+        pipeline_internal_register_map = new HashMap<String, Integer>() {{
+            put("IF/ID.IR", 0);
+            put("IF/ID.NPC", 1);
+            put("PC", 2);
+            put("ID/EX.A", 3);
+            put("ID/EX.B", 4);
+            put("ID/EX.IMM", 5);
+            put("ID/EX.IR", 6);
+            put("ID/EX.NPC", 7);
+            put("EX/MEM.ALUOutput", 8);
+            put("EX/MEM.IR", 9);
+            put("EX/MEM.N", 10);
+            put("EX/MEM.COND", 11);
+            put("MEM/WB.LMD", 12);
+            put("MEM/WB.IR", 13);
+            put("MEM/WB.ALUOutput", 14);
+            put("MEM[EX/MEM.ALUOutput", 15);
+        }};
     }
     
-    public void Start()
+    public void Cycle()
     {
-        //start counting
+        cycles++;
+        //extract instruction using current NPC
         InstructionFetch();
+        InstructionDecode();
     }
     
     public String GetJTableValue(JTable targetTable, int row, int column)
@@ -55,22 +96,45 @@ public class Pipeline {
         //IR <-- Mem[PC}
         //NPC <-- PC + 4
         String current_pc_hex = GetJTableValue(tableRegister, 32, 2);
+        
+        int ir_row_index = pipeline_internal_register_map.get("PC");
+        pipeline_internal_register_model.setValueAt(current_pc_hex, ir_row_index, 1);
+        
         int current_pc_int = Convert.HexToDecimal(current_pc_hex);
         
-        String ir = GetJTableValue(tableProgram, current_pc_int, 1);
+        String ir_opcode = GetJTableValue(tableProgram, current_pc_int, 1);
+        ir_row_index = pipeline_internal_register_map.get("IF/ID.IR");
+        pipeline_internal_register_model.setValueAt(ir_opcode, ir_row_index, 1);
         
         int next_pc = current_pc_int + 4;
-        int[] next_pc_bin = Convert.DecimalToBinary(current_pc_hex, 32); //hex value has 32 bits
+        int[] next_pc_bin = Convert.IntDecimalToBinary(next_pc, 32); //hex value has 32 bits
         String next_pc_hex = Convert.BinaryToHex(next_pc_bin);
         
-        register_model.setValueAt("0x"+next_pc_hex, 0, 0);
+        ir_row_index = pipeline_internal_register_map.get("IF/ID.NPC");
+        pipeline_internal_register_model.setValueAt(next_pc_hex, ir_row_index, 1);
+        
+        register_model.setValueAt(next_pc_hex, 32, 0);
     }
     
     //ID
     public void InstructionDecode(){
         //A <- Regs[IR 19...15]
         //B <- Regs[IR 24...20]
-        //Imm <- {Sign_extend[IR 31..20] (immediate ALU) | sign_extend[IR 31..25, 11..7] (branch/store)}
+        //Imm <- {Sign_extend[IR 31..20] (immediate ALU) | sign_extend[IR 31..25, 11..7] (branch/store
+        int ir_row_index = pipeline_internal_register_map.get("IF/ID.IR");
+        String p_ir_model_hex = GetJTableValue(tablePipelineInternalRegister, ir_row_index, 1);
+        int p_ir_model_int = Convert.HexToDecimal(p_ir_model_hex);
+        
+        if (p_ir_model_int != 0)
+        {
+            ir_row_index = pipeline_internal_register_map.get("IF/ID.NPC");
+            String current_pc_hex = GetJTableValue(tablePipelineInternalRegister, ir_row_index, 1);
+
+            ir_row_index = pipeline_internal_register_map.get("ID/EX.NPC");
+            pipeline_internal_register_model.setValueAt(current_pc_hex, ir_row_index, 1);
+            
+            
+        }
     }
     
     //EX
@@ -84,7 +148,18 @@ public class Pipeline {
         //Regs[11..7] <- ALU Output
     }
     
+    public int GetRegisterMapIndex(String register_name)
+    {
+        return pipeline_internal_register_map.get(register_name);
+    }
     
+    public static void AddBinaryToOpcode(int[] opcode_to_apply, int[] binary_opcode, int upper_bound, int lower_bound)
+    {
+        for (int i = upper_bound, j = 0; i >= lower_bound; i--,j++)
+        {
+            opcode_to_apply[31-i] = binary_opcode[j];
+        }
+    }
     
     public void load_word(String rd, String rs1)
     {
